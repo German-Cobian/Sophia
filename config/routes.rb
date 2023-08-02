6bdf9201947b98f97d6df3ca888c657a2b86d787
German Renato Cobian <germancobian@hotmail.com>
2023-08-01 19:13:23 -0700

    Implements routes, controller, and input form for username

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   get :dashboard, to: "dashboard#index"
-
+  resources :usernames, only: [:new, :update]
 end
