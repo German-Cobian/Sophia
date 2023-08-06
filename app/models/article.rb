3f85d675bb34b9001716f66c563e3c663d545791
German Renato Cobian <germancobian@hotmail.com>
2023-08-06 11:04:35 -0700

    Link Categories to Articles tables + add relationships and validations to respective models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
+  belongs_to :category, class_name: 'Category', foreign_key: :category_id
 
   validates :title, presence: true
   validates :body, presence: true
+  validates :category_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_06_050647) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_06_164752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_06_050647) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -42,5 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_06_050647) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "categories"
   add_foreign_key "articles", "users"
 end
