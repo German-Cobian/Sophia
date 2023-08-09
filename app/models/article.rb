ac1847aa58e1c13377f12e30ffeb03d43757c044
German Renato Cobian <germancobian@hotmail.com>
2023-08-08 22:58:13 -0700

    Implemented scope and relationships so that most recent article by category could be displayed in categories_index

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,21 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   validates :category_id, presence: true
+
+  scope :most_recent_by_category, lambda {
+    from(
+      <<~SQL
+        (
+          SELECT articles.*
+          FROM articles JOIN (
+            SELECT category_id, max(created_at) AS created_at
+            FROM articles
+            GROUP BY category_id
+          ) latest_by_category
+          ON articles.created_at = latest_by_category.created_at
+          AND articles.category_id = latest_by_category.category_id
+        ) articles
+      SQL
+    )
+  }
 end
