ddb797f3de521004d2b41b7db7d31a47d085affc
German Renato Cobian <germancobian@hotmail.com>
2023-08-08 23:20:28 -0700

    Eliminated home and dashboard view routing their previous functions to categories_index + Styling of said file

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
-  get :dashboard, to: "dashboard#index"
+  root "categories#index"
+  get :dashboard, to: "categories#index"
+
   resources :usernames, only: [:new, :update]
   resources :articles, only: [:index, :new, :create]
   resources :categories, only: [:index, :show]
