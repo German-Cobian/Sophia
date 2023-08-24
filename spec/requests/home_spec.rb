b62a7079078225ef63c9d2eb47e64deb2edc5479
German Renato Cobian <germancobian@hotmail.com>
2023-08-24 13:56:36 -0700

    Resolve linter issues

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    it "is succesful" do
+RSpec.describe 'Homes', type: :request do
+  describe 'GET /index' do
+    it 'is succesful' do
       get root_path
       expect(response).to have_http_status(:success)
     end
