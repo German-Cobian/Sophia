c18f923aa79160ba1ba53a19c98552216e5f106a
German Renato Cobian <germancobian@hotmail.com>
2023-09-02 15:55:58 -0700

    Implement category_controller methods and routes for deleting categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,6 +25,7 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
+    @category = Category.find(params[:id])
     @category.destroy
     respond_to do |format|
       format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
  
   resources :usernames, only: [:new, :update]
   resources :articles, only: [:index, :new, :show, :create]
-  resources :categories, only: [:index, :show, :new, :create]
+  resources :categories, only: [:index, :show, :new, :create, :destroy]
   resources :votes, only: [:create, :destroy]
   resources :articles do
     resources :comments, only: [:index, :create]
