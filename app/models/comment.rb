2bcde4bd54649172680beb4315b1d8adb3e4ba25
German Renato Cobian <germancobian@hotmail.com>
2023-09-07 14:25:21 -0700

    Add parent_id column to Comment model + Add relationships to the same model + Modify comments/show so that it prints out parent comment with nested comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :commenter, class_name: 'User', foreign_key: :user_id
+  belongs_to :parent, class_name: 'Comment', optional: true
+
+  has_many :comments, foreign_key: :parent_id
 
   validates_presence_of :body, length: { minimum: 3, maximum: 500 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_042541) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_201649) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_042541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "parent_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
