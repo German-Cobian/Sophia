2545575727203f8a1136ce082223084379b42c21
German Renato Cobian <germancobian@hotmail.com>
2023-09-21 13:48:29 -0700

    Implement counter_caches for following and followers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_010153) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_202233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_010153) do
     t.string "city"
     t.string "country"
     t.text "bio"
+    t.integer "following_count", default: 0, null: false
+    t.integer "follower_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
