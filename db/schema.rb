adb159a653f97f481f1daf926d72be2be8bc06cf
German Renato Cobian <germancobian@hotmail.com>
2023-09-21 17:57:57 -0700

    Create Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_202233) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_002450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_202233) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
   create_table "followings", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "following_user_id", null: false
@@ -118,6 +128,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_202233) do
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "events", "users"
   add_foreign_key "followings", "users"
   add_foreign_key "followings", "users", column: "following_user_id"
   add_foreign_key "votes", "articles"
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :event do
     title { "MyString" }
-    description { "MyString" }
-    date { "2023-09-18" }
-    location { "MyString" }
-    link { "MyString" }
+    description { "MyText" }
+    date { "2023-09-21" }
     user { nil }
   end
 end
